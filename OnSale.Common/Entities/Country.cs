3fe923d11da5900dbf1f3a97ebfad73f5ccd6eff
Edson Porres <Edsonporres@gmail.com>
2020-09-08 20:17:24 -0600

    Improve countries CRUD

---
+++ OnSale.Common/Entities/Country.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace OnSale.Common.Entities
+{
+    public class Country
+    {
+        
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50, ErrorMessage ="The field {0} must contain less than {1} characteres")]
+        
+        public string Name { get; set; }
+
+    }
+}
