7f96d3c68f6a14c7bf7bbf074e2fc89f6d1e3bc6
Edson Porres <Edsonporres@gmail.com>
2020-09-08 22:37:07 -0600

    Add department to a country

--- OnSale.Common/Entities/City.cs
+++ OnSale.Common/Entities/City.cs
@@ -1,24 +1,21 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel;
+﻿using Newtonsoft.Json;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OnSale.Common.Entities
 {
-    public class Country
+    public class City
     {
 
         public int Id { get; set; }
 
         [Required]
         [MaxLength(50, ErrorMessage = "The field {0} must contain less than {1} characteres")]
-
         public string Name { get; set; }
 
-        public ICollection<Department> Departments { get; set; }
-
-        [DisplayName("Departments Number")]
-        public int DepartmentsNumber => Departments == null ? 0 : Departments.Count;
-
+        [JsonIgnore]
+        [NotMapped]
+        public int IdDepartment { get; set; }
 
     }
 }
---
+++ OnSale.Web/Migrations/20200909024005_AddDepartmentsAndCities.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnSale.Web.Migrations
+{
+    public partial class AddDepartmentsAndCities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 50, nullable: false),
+                    CountryId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Departments_Countries_CountryId",
+                        column: x => x.CountryId,
+                        principalTable: "Countries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "cities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 50, nullable: false),
+                    DepartmentId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_cities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_cities_Departments_DepartmentId",
+                        column: x => x.DepartmentId,
+                        principalTable: "Departments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_cities_DepartmentId",
+                table: "cities",
+                column: "DepartmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_cities_Name",
+                table: "cities",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Departments_CountryId",
+                table: "Departments",
+                column: "CountryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Departments_Name",
+                table: "Departments",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "cities");
+
+            migrationBuilder.DropTable(
+                name: "Departments");
+        }
+    }
+}
